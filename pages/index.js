7647adf4b5c1fa7510fef766578045c5ee8c66cb
fuurin <komatsuemgbr0624@gmail.com>
2021-02-01 01:46:03 +0900

    useMemo

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,25 @@
-import React, {useState, useCallback} from 'react'
+import React, {useMemo, useState} from 'react'
 
-const Title = React.memo(() => {
-  console.log('Title component')
-  return <h2>useCallBack Test</h2>
-})
+export default function UseMemo() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
 
-const Button = React.memo(({handleClick,value}) => {
-  console.log('Button child component', value)
-  return <button type="button" onClick={handleClick}>{value}</button>
-})
+  const increment1 = () => setCount1(count1 + 1)
+  const increment2 = () => setCount2(count2 + 1)
 
-const Count = React.memo(({text, countState}) => {
-  console.log('Count child component', text)
-  return <p>{text}:{countState}</p>
-})
-
-export default function Counter() {
-  const [firstCountState, setFirstCountState] = useState(0)
-  const [secondCountState, setSecondCountState] = useState(10)
-
-  const incrementFirstCounter = useCallback(() => setFirstCountState(firstCountState + 1), [firstCountState])
-  const incrementSecondCounter = useCallback(() => setSecondCountState(secondCountState + 10), [secondCountState])
+  const square = useMemo(() => {
+    console.log('execute a heavy function square(no memo)')
+    let i = 0
+    while (i < 2000000000) i++
+    return count2 * count2
+  }, [count2])
 
   return <>
-    <Title/>
-    <Count text="+ 1 ボタン" countState={firstCountState}/>
-    <Count text="+ 10 ボタン" countState={secondCountState}/>
-    <Button handleClick={incrementFirstCounter} value={'+1 ボタン'}/>
-    <Button handleClick={incrementSecondCounter} value={'+10 ボタン'}/>
+    <div>count1: {count1}</div>
+    <div>count2: {count2}</div>
+    {/* useMemoでラップすると square() から square のように書き方が変わる*/}
+    <div>square of count2: {square}</div>
+    <button onClick={increment1}>increment1</button>
+    <button onClick={increment2}>increment2</button>
   </>
 }
